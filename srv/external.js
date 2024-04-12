9f5380b3b11bc6b181134b492bc89807e50ec4dd
Gayathri Madupu <gayathri.madupu@artihcus.com>
2024-04-12 10:16:36 +0000

    External sap cap

--- srv/external.js
+++
@@ -1,8 +0,0 @@
-const cds = require("@sap/cds");
-module.exports = cds.service.impl(async function() {
-    // Connect Service API_BUSINESS_PARTNER
-    const bp = await cds.connect.to("API_BUSINESS_PARTNER");
-    this.on("READ", "A_BusinessPartner", async req => {
-        return bp.run(req.query);
-    })
-})
